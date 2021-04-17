a37aaa475e21a2327119fbfdb36336a19e89042f
Kristina Andreeva <_>
2021-04-17 23:32:32 +0300

    refactor: utils

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,17 @@
-export const getRandomNumber = (min: number, max: number) => {
-	return Math.round(Math.random() * (max - min) + min);
+import { IRatingResponse } from '../api/api';
+
+export const calculateRating = (array: IRatingResponse[]) => {
+  const ratingsArray = array.map((obj) => obj.rating);
+  const reduce = ratingsArray.reduce((sum, current) => sum + current, 0);
+  return +(reduce / ratingsArray.length).toFixed(1);
 };
+
+export const nameRateArray = (array: IRatingResponse[]) => {
+  const nameRate = array.map((obj) => ({
+    name: obj.userName,
+    rate: obj.rating,
+  }));
+  return nameRate.filter(({ name }) => name !== undefined);
+};
+
+export const getRandomNumber = (min: number, max: number) => Math.round(Math.random() * (max - min) + min);
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,12 +1,12 @@
-
 const validation = (e: any, type: string, rule: any) => {
-    let invalid = true;
-    if (type === 'email') {
-        invalid = e.target.validity.typeMismatch;
-    } else {
-        invalid = e.target.value.length < 6
-    }
-    invalid ? e.target.setCustomValidity(rule) :
-        e.target.setCustomValidity("");
-}
+  let invalid = true;
+  if (type === 'email') {
+    invalid = e.target.validity.typeMismatch;
+  } else {
+    invalid = e.target.value.length < 6;
+  }
+  if (invalid) {
+    e.target.setCustomValidity(rule);
+  } else e.target.setCustomValidity('');
+};
 export default validation;
