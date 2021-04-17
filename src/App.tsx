6e5ef1f3c82a8f04ab7785aedf50d29d037972a7
Kristina Andreeva <_>
2021-04-17 23:33:32 +0300

    refactor: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import React, { useEffect } from "react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
-import "./css/App.scss";
-import Main from "./pages/Main";
-import Countries from "./pages/Countries";
-import { useDispatch, useSelector } from "react-redux";
-import { setCountriesThunk } from "./redux/thunk/thunk";
-import Signup from "./components/Auth/Signup";
-import Login from "./components/Auth/Login";
-import { IState } from "./redux/reducers/reducerTypes";
-
+import React, { useEffect } from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import './css/App.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import Main from './pages/Main';
+import Countries from './pages/Countries';
+import { setCountriesThunk } from './redux/thunk/thunk';
+import Signup from './components/Auth/Signup';
+import Login from './components/Auth/Login';
+import { IState } from './redux/reducers/reducerTypes';
 
 const App = () => {
   const dispatch = useDispatch();
-  const lang = useSelector((state: IState) => state.lang)
+  const lang = useSelector((state: IState) => state.lang);
 
   useEffect(() => {
     dispatch(setCountriesThunk());
