05a33b397577007a09463ca7f378307d4aa15ee1
Kristina Andreeva <_>
2021-04-22 20:18:54 +0300

    refactor card country style

--- src/components/CountriesList/CountriesList.tsx
+++ src/components/CountriesList/CountriesList.tsx
@@ -19,7 +19,7 @@ const CountryCard = (props: Props) => {
           <Link to={`/${country.ISOCode}`} key={country.ISOCode}>
             <div className={classes.country}>
               <img className={classes.countryImage} src={`${country.imageURL}?fit=crop&w=500`} alt={country.country} />
-              <span className={classes.countryTitle}>{country.country}</span>
+              <div className={classes.countryTitle}>{country.country}</div>
             </div>
           </Link>
         ))}
